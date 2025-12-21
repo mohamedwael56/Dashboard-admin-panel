21093f248a63fdbbe7996418e011beff16627ea7
mohamed wael <mohmedhomos0147@gmail.com>
2025-12-21 22:30:30 +0200

    divide the header and the products list and the side bar in separate components

---
+++ Dashboard-project/src/components/Products.jsx
@@ -0,0 +1,258 @@
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { faPen } from "@fortawesome/free-solid-svg-icons";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+export function Product (){
+    return (
+        <>
+        <div className="w-full ">
+  <div className="grid grid-cols-[1fr_10fr_2fr_2fr_2fr_2fr_2fr_2fr_2fr_2fr] 
+   gap-4 font-semibold border-b p-1 border bg-blue-500 rounded-lg text-white  ">
+    <div className="flex start">UID</div>
+    <div className=" flex justify-start ">Product</div>
+    <div>Category </div>
+    <div>brand</div>
+    <div>price</div>
+    <div>stock</div>
+    <div>rating</div>
+    <div>order</div>
+    <div className="mr-4">sales</div>
+    <div className="mr-4">action</div>
+  </div>
+<div className="grid grid-cols-[1fr_10fr_2fr_2fr_2fr_2fr_2fr_2fr_2fr_2fr] gap-4 p-2">
+  <div className="flex start">
+   1
+  </div>
+
+  <div className=" flex justify-start items-center ">
+
+<img src="../../image/jacket.jpg" alt="" width='35px' />
+
+<div className="text-sm ml-2 ">
+<div > Waterproof jacket with zipper and hood</div>
+<div className="flex justify-start opacity-80 ">winter clothes </div>
+
+</div>
+  </div>
+  <div>
+women
+  </div>
+  <div>
+  richman
+  </div>
+  <div>
+  $30.10
+  </div>
+  <div>
+  44
+  </div>
+  <div >
+    <div className="flex items-center gap-1">
+<img src="../../public/image/star.png" alt=""  width="10px"/>
+<span className="text-sm">4.9 </span>
+<span>(16)</span>
+</div>
+  </div>
+  <div>
+  180
+  </div>
+  <div>
+  $38k
+  </div>
+  <div className="flex items-center gap-1" >
+  <a href="#" className="bg-violet-400 text-white p-1 rounded-xl " ><FontAwesomeIcon icon={faEye} /></a>
+  <a href="#" className="bg-green-300 text-white p-1 rounded-xl "><FontAwesomeIcon icon={faPen} /></a>
+  <a href="#" className="bg-red-500 text-white p-1 rounded-xl "><FontAwesomeIcon icon={faTrashCan} /></a>
+  </div>
+</div>
+ 
+<div className="grid grid-cols-[1fr_10fr_2fr_2fr_2fr_2fr_2fr_2fr_2fr_2fr] gap-4 p-2">
+  <div className="flex start">
+ 2
+  </div>
+   <div className=" flex justify-start items-center ">
+
+<img src="../../image/jacket.jpg" alt="" width='35px' />
+
+<div className="text-sm ml-2 ">
+<div > Waterproof jacket with zipper and hood</div>
+<div className="flex justify-start opacity-80 ">winter clothes </div>
+
+</div>
+  </div>
+  <div>
+men
+  </div>
+  <div>
+  ecstasy
+  </div>
+
+  <div>
+  $15.12
+  </div>
+  <div>
+  34
+  </div>
+  <div >
+    <div className="flex items-center gap-1">
+<img src="../../public/image/star.png" alt=""  width="10px"/>
+<span className="text-sm">4.9 </span>
+<span>(16)</span>
+</div>
+  </div>
+  <div>
+  350
+  </div>
+  <div>
+  $27k
+  </div>
+  <div className="flex items-center gap-1" >
+  <a href="#" className="bg-violet-400 text-white p-1 rounded-xl " ><FontAwesomeIcon icon={faEye} /></a>
+  <a href="#" className="bg-green-300 text-white p-1 rounded-xl "><FontAwesomeIcon icon={faPen} /></a>
+  <a href="#" className="bg-red-500 text-white p-1 rounded-xl "><FontAwesomeIcon icon={faTrashCan} /></a>
+  </div>
+</div>
+
+<div className="grid grid-cols-[1fr_10fr_2fr_2fr_2fr_2fr_2fr_2fr_2fr_2fr] gap-4 p-2">
+  <div className="flex start"> 
+ 3
+  </div>
+   <div className=" flex justify-start items-center ">
+
+<img src="../../image/jacket.jpg" alt="" width='35px' />
+
+<div className="text-sm ml-2 ">
+<div > Waterproof jacket with zipper and hood</div>
+<div className="flex justify-start opacity-80 ">winter clothes </div>
+
+</div>
+  </div>
+  <div>
+kids
+  </div>
+  <div>
+ecstasy
+  </div>
+
+  <div>
+  $60.25
+  </div>
+  <div>
+  50
+  </div>
+   <div >
+    <div className="flex items-center gap-1">
+<img src="../../public/image/star.png" alt=""  width="10px"/>
+<span className="text-sm">4.9 </span>
+<span>(16)</span>
+</div>
+  </div>
+  <div>
+  145
+  </div>
+  <div>
+  $23k
+  </div>
+  <div className="flex items-center gap-1" >
+  <a href="#" className="bg-violet-400 text-white p-1 rounded-xl " ><FontAwesomeIcon icon={faEye} /></a>
+  <a href="#" className="bg-green-300 text-white p-1 rounded-xl "><FontAwesomeIcon icon={faPen} /></a>
+  <a href="#" className="bg-red-500 text-white p-1 rounded-xl "><FontAwesomeIcon icon={faTrashCan} /></a>
+  </div>
+</div>
+<div className="grid grid-cols-[1fr_10fr_2fr_2fr_2fr_2fr_2fr_2fr_2fr_2fr] gap-4 p-2">
+  <div className="flex start">
+ 4
+  </div>
+  <div className=" flex justify-start items-center ">
+
+<img src="../../image/jacket.jpg" alt="" width='35px' />
+
+<div className="text-sm ml-2 ">
+<div > Waterproof jacket with zipper and hood</div>
+<div className="flex justify-start opacity-80 ">winter clothes </div>
+
+</div>
+  </div>
+  <div>
+men  </div>
+  <div>
+  lubana
+  </div>
+
+  <div>
+  $17.80
+  </div>
+  <div>
+  43
+  </div>
+  <div >
+    <div className="flex items-center gap-1">
+<img src="../../public/image/star.png" alt=""  width="10px"/>
+<span className="text-sm">4.9 </span>
+<span>(16)</span>
+</div>
+  </div>
+  <div>
+ 196
+  </div>
+  <div>
+  $18k
+  </div>
+  <div className="flex items-center gap-1" >
+  <a href="#" className="bg-violet-400 text-white p-1 rounded-xl " ><FontAwesomeIcon icon={faEye} /></a>
+  <a href="#" className="bg-green-300 text-white p-1 rounded-xl "><FontAwesomeIcon icon={faPen} /></a>
+  <a href="#" className="bg-red-500 text-white p-1 rounded-xl "><FontAwesomeIcon icon={faTrashCan} /></a>
+  </div>
+</div>
+<div className="grid grid-cols-[1fr_10fr_2fr_2fr_2fr_2fr_2fr_2fr_2fr_2fr] gap-4 p-2">
+  <div className="flex start">
+   5
+  </div>
+   <div className=" flex justify-start items-center ">
+
+<img src="../../image/jacket.jpg" alt="" width='35px' />
+
+<div className="text-sm ml-2 ">
+<div > Waterproof jacket with zipper and hood</div>
+<div className="flex justify-start opacity-80 ">winter clothes </div>
+
+</div>
+  </div>
+  <div>
+women
+  </div>
+  <div>
+ecstasy
+  </div>
+
+  <div>
+  $12.15
+  </div>
+  <div>
+  23
+  </div>
+  <div >
+    <div className="flex items-center gap-1">
+<img src="../../public/image/star.png" alt=""  width="10px"/>
+<span className="text-sm">4.9 </span>
+<span>(16)</span>
+</div>
+  </div>
+  <div>
+  165
+  </div>
+  <div>
+  $15k
+  </div>
+  <div className="flex items-center gap-1" >
+  <a href="#" className="bg-violet-400 text-white p-1 rounded-xl " ><FontAwesomeIcon icon={faEye} /></a>
+  <a href="#" className="bg-green-300 text-white p-1 rounded-xl "><FontAwesomeIcon icon={faPen} /></a>
+  <a href="#" className="bg-red-500 text-white p-1 rounded-xl "><FontAwesomeIcon icon={faTrashCan} /></a>
+  </div>
+</div>
+
+
+</div>
+  </>
+    )
+}
---
+++ Dashboard-project/src/components/SideBar.jsx
@@ -0,0 +1,79 @@
+export function SideBar (){
+    return(
+            <aside className="w-64 h-screen bg-gray-100 text-black-100 flex flex-col p-4">
+          <h2 className="text-s font-bold-1 mb-6 select-none flex start opacity-80">
+            Main page
+          </h2>
+
+          <nav className="flex flex-col gap-3 capitalize">
+            <a
+              href="#"
+              className="flex items-center px-3 py-2 rounded hover:bg-gray-200 "
+            >
+              <span></span>
+              <span className="ml-2">Dashboard</span>
+            </a>
+            <div className="flex items-center hover:bg-gray-200 hover:rounded-lg">
+            <details className="flex items-center px-3 py-2 rounded  inline-block select-none ">
+              <summary className="ml-2    text-black">Authentication </summary>
+              <div className="flex flex-col ml-4 mt-2 gap-2">
+                <a href="Login" className="hover:underline hover:bg-gray-300">login</a>
+                <a href="Register" className="hover:underline hover:bg-gray-300">register</a>
+                <a href="ResetPassword" className="hover:underline hover:bg-gray-300">forgot password</a>
+              </div>
+            </details>
+            </div>
+            <a
+              href="#"
+              className="flex items-center px-3 py-2 rounded hover:bg-gray-200"
+            >
+              <span></span>
+              <span className="ml-2">users</span>
+            </a>
+            <a
+              href="#"
+              className="flex items-center px-3 py-2 rounded hover:bg-gray-200"
+            >
+              <span></span>
+              <span className="ml-2">products</span>
+            </a>
+            <a
+              href="#"
+              className="flex items-center px-3 py-2 rounded hover:bg-gray-200"
+            >
+              <span></span>
+              <span className="ml-2">invoices</span>
+            </a>
+            <a
+              href="#"
+              className="flex items-center px-3 py-2 rounded hover:bg-gray-200"
+            >
+              <span></span>
+              <span className="ml-2">orders</span>
+            </a>
+            <a
+              href="#"
+              className="flex items-center px-3 py-2 rounded hover:bg-gray-200"
+            >
+              <span></span>
+              <span className="ml-2">messages</span>
+            </a>
+            <a
+              href="#"
+              className="flex items-center px-3 py-2 rounded hover:bg-gray-200"
+            >
+              <span></span>
+              <span className="ml-2">notifications</span>
+            </a>
+            <a
+              href="#"
+              className="flex items-center px-3 py-2 rounded hover:bg-gray-200"
+            >
+              <span></span>
+              <span className="ml-2">settings</span>
+            </a>
+          </nav>
+        </aside>
+
+    )
+}
