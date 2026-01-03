3a039dcbc73cc937792562636bc48132d0a7189f
mohamed wael <mohmedhomos0147@gmail.com>
2026-01-03 16:38:57 +0200

    creating charts for the cards

--- Dashboard-project/src/pages/home/Home.jsx
+++ Dashboard-project/src/pages/home/Home.jsx
@@ -6,32 +6,86 @@ import { faBagShopping } from "@fortawesome/free-solid-svg-icons";
 import { Product } from "../../components/Products";
 import { Header } from "../../components/Header";
 import { SideBar } from "../../components/SideBar";
-import { BarChart, Bar, XAxis, Tooltip, ResponsiveContainer, YAxis } from "recharts";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  AreaChart,
+  Area,
+  LineChart,
+  Tooltip,
+  ResponsiveContainer,
+  YAxis,
+  Line,
+} from "recharts";
 
 export function Home() {
-
   const data = [
-  { name: "Jan", users: 100 },
-  { name: "Feb", users: 200 },
-  { name: "Mar", users: 300 },
-];
+    { name: "Jan", users: 100, orders: 90 , products: 10  , reviews: 10, sales:10000 },
+    { name: "Feb", users: 700, orders: 280, products: 100 , reviews: 50, sales:18000 },
+    { name: "Mar", users: 200, orders: 180, products: 500 , reviews: 100, sales:1000  },
+    { name: "Apr", users: 900, orders: 538, products: 1000, reviews: 165, sales:25300 },
+  ];
 
   return (
     <>
       <Header />
       <div className="flex min-h-screen mt-15">
-    <SideBar />
+        <SideBar />
         <main className="flex-1 p-6 m-7  w-screen">
           <div className="grid grid-cols-3 grid-rows-2 gap-4">
-            <button className="flex justify-between h-36 row-span-2 bg-gradient-to-r from-green-700 to-green-300 p-4 text-white border-none ">
-              <div className="flex flex-col justify-between h-full">
+            <button className="flex justify-between h-64 row-span-2 bg-gradient-to-r from-green-700 to-green-300 p-4 text-white border-none ">
+              <div className="flex w-full flex-col justify-between h-full items-start">
                 <div className="flex flex-col items-start">
                   <div>Total Users</div>
                   <div>277</div>
                 </div>
-                <div className="h-8  rounded-lg flex items-center justify-center text-gray-400">
-. </div>
-                <div>
+                <div className="h-64 w-full border-none rounded-lg flex items-center justify-center text-gray-400">
+                  <ResponsiveContainer width="100%" height={120}>
+                    <AreaChart data={data}>
+                      <defs>
+                        <linearGradient
+                          id="usersGradient"
+                          x1="0"
+                          y1="0"
+                          x2="0"
+                          y2="1"
+                        >
+                          <stop
+                            offset="5%"
+                            stopColor="#fff"
+                            stopOpacity={0.4}
+                          />
+                          <stop offset="95%" stopColor="#fff" stopOpacity={0} />
+                        </linearGradient>
+                      </defs>
+
+                      <XAxis dataKey="name" hide />
+
+                      <Tooltip
+                        contentStyle={{
+                          background: "#111",
+                          border: "none",
+                          borderRadius: "8px",
+                          color: "#fff",
+                        }}
+                        cursor={{ stroke: "rgba(255,255,255,0.2)" }}
+                      />
+
+                      <Area
+                        type="monotone"
+                        dataKey="users"
+                        stroke="#fff"
+                        fill="url(#usersGradient)"
+                        strokeWidth={3}
+                        dot={false}
+                        activeDot={{ r: 6 }}
+                      />
+                    </AreaChart>
+                  </ResponsiveContainer>
+                </div>
+
+                <div >
                   <span>+95% </span>
                   <span>last month</span>
                 </div>
@@ -42,20 +96,53 @@ export function Home() {
                 </a>
               </div>
             </button>
-            <button class="flex justify-between h-36 row-span-2 bg-gradient-to-r from-pink-500 to-pink-300 p-4 text-white">
-              <div className="flex flex-col justify-between h-full">
+            <button class="flex justify-between h-64 row-span-2 bg-gradient-to-r from-pink-500 to-pink-300 p-4 text-white">
+              <div className="flex w-full flex-col justify-between h-full items-start">
                 <div className="flex flex-col items-start">
                   <div>Total orders</div>
                   <div>338</div>
                 </div>
-                <div className="h-32 bg-gray-100 rounded-lg flex items-center justify-center text-gray-400">
-    Chart goes here
-  </div>
+                <div className="h-64 w-full rounded-lg flex items-center justify-center text-gray-400">
+                  <ResponsiveContainer width="100%" height="100%">
+                    <AreaChart data={data}>
+                    <defs>
+                      <linearGradient
+                      id="ordersGradient"
+                      x1="0"
+                      y1="0"
+                      x2="0"
+                      y2="1"
+                      >
+                        <stop offset="5%" stopColor="#fff" stopOpacity="0.4" />
+                        <stop offset="95%" stopColor="#fff" stopOpacity="0" />
+                      </linearGradient>
+                    </defs>
+                      <XAxis dataKey="name" hide />
+                      <Tooltip
+                        contentStyle={{
+                          background: "#111",
+                          color: "#fff",
+                          border: "none",
+                          borderRadius: "10px",
+                        }}
+                      />
+                      <Area
+                        dataKey="orders"
+                        type="monotone"
+                        stroke="#fff"
+                        dot={false}
+                        strokeWidth={3}
+                        fill="url(#ordersGradient)"
+                        radius={[6, 6, 0, 0]}
+                      />
+                    </AreaChart>
+                  </ResponsiveContainer>
+                </div>
+
                 <div>
                   <span>+30% </span>
                   <span>last month</span>
                 </div>
-                
               </div>
               <div>
                 <a href="#" className="cursor-pointer text-2xl select-none">
@@ -64,33 +151,87 @@ export function Home() {
               </div>
             </button>
             <button class=" capitalize row-span-4 bg-gradient-to-r from-blue-500 to-blue-200 p-6 text-white flex flex-col gap-2 items-start">
-              <div>
-                <div className="mb-1 capitalize">total sales</div>
-                <div>$25,300</div>
-                 <div className="h-64 w-full  rounded-lg flex items-center justify-center text-gray-400">
-<ResponsiveContainer width="100%" height="100%">
-  <BarChart data={data}>
-    <XAxis dataKey="name"  />
-    <Tooltip />
-    <Bar dataKey="users" fill="#fff" radius={[6,6,0,0]} />
-  </BarChart>
-</ResponsiveContainer> 
- </div>
-              </div>
-              <div>
-                <span>+55% </span>
-                <span>last month</span>
+              <div className="flex items-start flex-col h-full w-full justify-between">
+                <div>
+                  <div className="mb-1 capitalize">total sales</div>
+                  <div>$25,300</div>
+                </div>
+                <div className="h-64 w-full text-gray-400">
+                  <ResponsiveContainer width="100%" height="100%">
+                    <AreaChart data={data}>
+                    <defs>
+                      <linearGradient
+                      id="salesGradient"
+                      y1="0"
+                      x1="0"
+                      x2="0"
+                      y2="1"
+                      >
+                      <stop offset="5%" stopColor="#fff" stopOpacity="0.4" />
+                      <stop offset="95%" stopColor="#fff" stopOpacity="0" />
+                      </linearGradient>
+                    </defs>
+                      <XAxis dataKey="name" hide />
+                      <Tooltip contentStyle={{
+                        background:"#000",
+                        color:"#fff",
+                        border:"none",
+                        borderRadius:"10px"
+                      }} />
+                      <Area dataKey="sales"
+                      dot={false}
+                      strokeWidth={3}
+                      stroke="#fff"
+                      type="monotone"
+                      fill="url(#salesGradient)"
+                      radius={[6, 6, 0, 0]} />
+                    </AreaChart>
+                  </ResponsiveContainer>
+                </div>
+
+                <div>
+                  <span>+55% </span>
+                  <span>last month</span>
+                </div>
               </div>
             </button>
             <button class="flex justify-between row-span-2 bg-gradient-to-r from-blue-700 to-blue-300 p-4 text-white">
-              <div className="flex flex-col justify-between items-start">
+              <div className="flex flex-col w-full justify-between items-start">
                 <div className="flex flex-col items-start">
                   <div>Total products</div>
                   <div>557</div>
                 </div>
-                <div className="h-8 bg-gray-100 rounded-lg flex items-center justify-center text-gray-400">
-    Chart goes here
-  </div>
+                <div className="h-32 w-full rounded-lg flex items-center justify-center text-gray-400">
+                  <ResponsiveContainer width="100%" height="100%">
+                    <AreaChart data={data}>
+                    <defs>
+                      <linearGradient
+                      id="productsGradient"
+                      y1="0"
+                      y2="1"
+                      x1="0"
+                      x2="0">
+                        <stop stopColor="#fff" offset="5%" stopOpacity="0.4" />
+                        <stop stopColor="#fff" offset="95%" stopOpacity="0" />
+
+                      </linearGradient>
+                    </defs>
+                      <XAxis dataKey="name" hide />
+                      <Tooltip contentStyle={{
+                        color:"white",
+                        background:"black",
+                        border:"none",
+                        borderRadius:"10px"
+                      }} />
+                      <Area dataKey="products"
+                      type="monotone"
+                      stroke="#fff"
+                      strokeWidth={3}
+                      fill="url(#productsGradient)"
+                      radius={[6, 6, 0, 0]} />
+                    </AreaChart>
+                  </ResponsiveContainer>
+                </div>
                 <div>
                   <span>+25% </span>
                   <span>last month</span>
@@ -102,17 +243,46 @@ export function Home() {
                 </a>
               </div>
             </button>
-            <button class=" flex justify-between h-36 row-span-2 bg-gradient-to-r from-amber-500 to-amber-200 p-4 text-white">
-              <div className="flex flex-col justify-between items-start">
+            <button class=" flex justify-between h-64 row-span-2 bg-gradient-to-r from-amber-500 to-amber-200 p-4 text-white">
+              <div className="flex flex-col w-full justify-between items-start">
                 <div className="flex flex-col items-start">
                   <div>Total reviews</div>
                   <div>165</div>
                 </div>
-                <div className="h-32 bg-gray-100 rounded-lg w-full flex items-center justify-center text-gray-400">
- <div className="w-3 bg-black h-1/2 rounded"></div>
-  <div className="w-3 bg-black h-3/4 rounded"></div>
-  <div className="w-3 bg-black h-1/3 rounded"></div>
-  <div className="w-3 bg-black h-full rounded"></div>  </div>
+                <div className="h-64 w-full rounded-lg flex items-center justify-center text-gray-400">
+                  <ResponsiveContainer width="100%" height="100%">
+                    <AreaChart data={data}>
+                    <defs>
+                      <linearGradient 
+                      id="reviewsGradient"
+                      x1="0"
+                      y1="0"
+                      x2="0"
+                      y2="1"
+                      >
+                        <stop offset="5%" stopColor="white" stopOpacity="0.4" />
+                        <stop offset="95%" stopColor="white" stopOpacity="0" />
+                      </linearGradient>
+                    </defs>
+                      <XAxis dataKey="name" hide />
+                      <Tooltip contentStyle={{
+                        background:"black"
+                        , color:"white",
+                        border:"none",
+                        borderRadius:"10px"
+                      }} />
+                      <Area 
+                      dataKey="reviews"
+                      type="monotone"
+                      dot={false}
+                      stroke="white"
+                      strokeWidth={3}
+                      fill="url(#reviewsGradient)"
+                      radius={[6, 6, 0, 0]}
+                         />
+                    </AreaChart>
+                  </ResponsiveContainer>
+                </div>
                 <div>
                   <span>+45% </span>
                   <span>last month</span>
@@ -137,10 +307,10 @@ export function Home() {
 
                 <div>
                   <select className="bg-gray-200 p-2">
-                      <option value="10">row 10</option>
-                      <option value="20">row 20</option>
-                      <option value="50">row 50</option>
-                      <option value="100">row 100</option>
+                    <option value="10">row 10</option>
+                    <option value="20">row 20</option>
+                    <option value="50">row 50</option>
+                    <option value="100">row 100</option>
                   </select>
                 </div>
                 <div>
@@ -156,7 +326,8 @@ export function Home() {
                   </select>
                 </div>
                 <div>
-                  <input className="bg-gray-200 p-2 border-none rounded-lg"
+                  <input
+                    className="bg-gray-200 p-2 border-none rounded-lg"
                     type="text"
                     name=""
                     id=""
@@ -168,7 +339,6 @@ export function Home() {
           </div>
 
           <Product />
-
         </main>
       </div>
     </>
